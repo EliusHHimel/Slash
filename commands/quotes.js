6e490ed23c31384c704d66b647a6bf447e40523e
Glitch (glitch-hello-node) <none>
2022-12-17 19:32:50 +0000

    🤑👰 Checkpoint ./commands/quotes.js:47478600/5025

--- commands/quotes.js
+++ commands/quotes.js
@@ -1,15 +1,26 @@
-const { SlashCommandBuilder, EmbedBuilder  } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('quotes')
-		.setDescription('Gives a random quotes!'),
-	async execute(interaction) {
+  data: new SlashCommandBuilder()
+    .setName("quotes")
+    .setDescription("Gives a random quotes!"),
+  async execute(interaction) {
     let response = await fetch(`https://type.fit/api/quotes`);
-    let data = await response.json()
-    const randomQuoteIndex = Math.round(Math.random()*data.length);
+    let data = await response.json();
+    const randomQuoteIndex = Math.round(Math.random() * data.length);
     const quote = data[randomQuoteIndex];
-		await interaction.reply(`${quote.text} \n ~${quote.author}`);
-	},
+    const generateRandomHexColor = () =>
+  `#${Math.floor(Math.random() * 0xffffff).toString(16)}`
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(generateRandomHexColor())
+      .setAuthor({
+        name: `~${quote.author}`,
+      })
+      .setDescription(quote.text)
+      .setTimestamp()
+
+    await interaction.reply({ embeds: [exampleEmbed] });
+  },
 };
