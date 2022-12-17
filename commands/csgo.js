8b6d97c92e18589c4254a64b40578c861c82dd7c
Glitch (glitch-hello-node) <none>
2022-12-17 20:18:53 +0000

    👀🛣 Checkpoint ./commands/csgo.js:47478600/3523

--- commands/csgo.js
+++ commands/csgo.js
@@ -1,37 +1,49 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
-const getSteamID64 = require('customurl2steamid64');
-const steam = require('steam-web');
+const getSteamID64 = require("customurl2steamid64");
+const steam = require("steam-web");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("csgo")
     .setDescription("Get your CS:GO stats!")
-    .addUserOption((option) => option.setName('steamid').setDescription('Steam ID of the person').setRequired(true)),
+    .addUserOption((option) =>
+      option
+        .setName("steamid")
+        .setDescription("Steam ID of the person")
+        .setRequired(true)
+    ),
   async execute(interaction) {
-    
-    var username = 'yllanos';
+    var username = "EliusHHimel";
     var baseURL = "http://steamcommunity.com/id/";
-    var URL = baseURL + username +  "/?xml=1";
+    var URL = baseURL + username + "/?xml=1";
     var s = new steam({
-  apiKey: 'D0BB2B1F8528C56E16C31FC7C005737B', // <<--PROVIDE API KEY HERE
-  format: 'json' //optional ['json', 'xml', 'vdf']
-  });
-      
+      apiKey: "D0BB2B1F8528C56E16C31FC7C005737B", // <<--PROVIDE API KEY HERE
+      format: "json", //optional ['json', 'xml', 'vdf']
+    });
+    getSteamID64(URL).then(function (result) {
+      s.getPlayerSummaries({
+        steamids: [result],
+        callback: function (err, data) {
+          console.log(data["response"]["players"]);
+        },
+      });
+    });
+
     let response = await fetch(`https://type.fit/api/quotes`);
     let data = await response.json();
     const randomQuoteIndex = Math.round(Math.random() * data.length);
     const quote = data[randomQuoteIndex];
     const generateRandomHexColor = () =>
-  `#${Math.floor(Math.random() * 0xffffff).toString(16)}`
+      `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
     const exampleEmbed = new EmbedBuilder()
       .setColor(generateRandomHexColor())
       .setAuthor({
         name: `~${quote.author}`,
       })
       .setDescription(quote.text)
-      .setTimestamp()
+      .setTimestamp();
 
     await interaction.reply({ embeds: [exampleEmbed] });
   },
