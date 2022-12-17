23b14c24647fe75a400e266678f18d23b5d2e742
Glitch (glitch-hello-node) <none>
2022-12-17 18:21:50 +0000

    🐷🤘 Checkpoint ./commands/ping.js:47478600/240 ./commands/server.js:47478600/435 ./commands/user.js:47478600/529 ./server.js:47478600/67

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const token = process.env.TOKEN;
+console.log(process.env.TOKEN)
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
