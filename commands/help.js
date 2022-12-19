b079f1b17c0af139136819c86d9369e4b07d80c7
Glitch (glitch-hello-node) <none>
2022-12-19 06:16:00 +0000

    🍉🍥 Checkpoint ./commands/help.js:47478600/755

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
 	async execute(interaction) {
     const generateRandomHexColor = () =>
       `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
-    const movieEmbed = new EmbedBuilder()
+    const helpEmbed = new EmbedBuilder()
       .setColor(generateRandomHexColor())
       .setTitle('Slash Help')
       .setDescription('See all the available command list')
@@ -16,10 +16,11 @@ module.exports = {
 		    { name: 'Ping', value: "Run this command to see the bot ping \n ```/ping```"},
         { name: 'Movie', value: "Run this command to search a movie information \n ```/movie `movie_title: Interstellar````" },
         { name: 'Quotes', value: "Run this command to get a random quotes \n ```/quotes```" },
-        { name: 'Awards', value: movieData.Awards },
+        { name: 'CS:GO', value: "Run this command to see your CS:GO Stats \n ```/csgo `steamid: officials1mple````" },
+        { name: 'Server', value: "Run this command to see the server info \n ```/server```" },
 	)
       .setTimestamp()
       .setFooter({ text: 'Slash', iconURL: 'https://cdn.discordapp.com/attachments/690148635375435825/1054266142283284510/Slash.png' });
-		await interaction.reply("```"+"My Latency:  " + ping + "/ms"+"```");
+		await interaction.reply({embeds: [helpEmbed]});
 	},
 };
