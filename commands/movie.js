13572004cffe0d0d2224680374e11390265168ca
Glitch (glitch-hello-node) <none>
2022-12-19 05:16:02 +0000

    😎🏖 Checkpoint ./commands/movie.js:47478600/1071

--- commands/movie.js
+++ commands/movie.js
@@ -29,17 +29,25 @@ module.exports = {
 
     const generateRandomHexColor = () =>
       `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
-    const movieRuntime = `${Math.f}`
-
+    const movieRuntimeInt = parseInt(movieData.Runtime)
+    const movieRuntime = `${Math.floor(movieRuntimeInt/60)} Hrs, ${movieRuntimeInt%60} Mins`
     const exampleEmbed = new EmbedBuilder()
       .setColor(generateRandomHexColor())
+      .setImage(movieData.Poster)
       .setTitle(movieData.Title)
     	.addFields(
-		{ name: 'Genre', value: movieData.Genre },
-		{ name: 'Year', value: movieData.Year, inline: true },
-		{ name: 'Runtime', value: 'Some value here', inline: true },
+		    { name: 'Genre', value: movieData.Genre },
+		    { name: 'Year', value: movieData.Year, inline: true },
+		    { name: 'Runtime', value: movieRuntime, inline: true },
+        { name: 'Director', value: movieData.Director, inline: true },
+        { name: 'Actors', value: movieData.Actors },
+        { name: 'Writer', value: movieData.Writer },
+        { name: 'Awards', value: movieData.Awards },
+        { name: 'IMDB Rating', value: movieData.imdbRating, inline: true },
+		    { name: 'IMDB Votes', value: movieData.imdbVotes, inline: true },
+        { name: 'Box Office', value: movieData.BoxOffice, inline: true },
 	)
-      .setDescription("CS:GO Player Info")
+      .setDescription(movieData.Plot)
       .setTimestamp();
 
     await interaction.reply("```" + "My Latency:  " + "/ms" + "```");
