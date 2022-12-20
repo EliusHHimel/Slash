88048af8a3c77c60c8b6a97c8326bb986b713232
Glitch (slashdiscord) <none>
2022-12-20 09:44:19 +0000

    🏃🏦 Checkpoint ./commands/mc.js:47478600/1377

--- commands/mc.js
+++ commands/mc.js
@@ -2,6 +2,7 @@ const {
   Client,
   SlashCommandBuilder,
   GatewayIntentBits,
+  EmbedBuilder
 } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const fetch = (...args) =>
@@ -30,8 +31,30 @@ module.exports = {
     const url = "https://api.mcsrvstat.us/2/"+ip+':'+ port ? port : '';
     const getServerStatus = await fetch(url);
     const serverData = await getServerStatus.json();
+    
+    const generateRandomHexColor = () =>
+      `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
 
-  
-    await interaction.reply("```" + "My Latency:  " + "/ms" + "```");
+  const serverEmbed = new EmbedBuilder()
+      .setColor(generateRandomHexColor())
+      .setThumbnail(serverData.favicon)
+      .setTitle('Server Status')
+    	.addFields(
+		    { name: 'Genre', value: movieData.Genre },
+		    { name: 'Year', value: movieData.Year, inline: true },
+		    { name: 'Runtime', value: movieRuntime, inline: true },
+        { name: 'Director', value: movieData.Director, inline: true },
+        { name: 'Actors', value: movieData.Actors },
+        { name: 'Writer', value: movieData.Writer },
+        { name: 'Awards', value: movieData.Awards },
+        { name: 'IMDB Rating', value: movieData.imdbRating, inline: true },
+		    { name: 'IMDB Votes', value: movieData.imdbVotes, inline: true },
+        { name: 'Box Office', value: movieData.BoxOffice ? movieData.BoxOffice : 'N/A', inline: true },
+	)
+      .setDescription(movieData.Plot)
+      .setTimestamp()
+      .setFooter({ text: 'Slash', iconURL: 'https://cdn.discordapp.com/attachments/690148635375435825/1054266142283284510/Slash.png' });
+
+    await interaction.reply({ embeds: [serverEmbed] });
   },
 };
