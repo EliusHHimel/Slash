b9ac9df1daa9ff4883d2f7f884bd564dcd7eb5bf
Glitch (slashdiscord) <none>
2022-12-20 08:11:14 +0000

    🐇😼 Checkpoint ./commands/mc.js:47478600/4254

--- commands/mc.js
+++ commands/mc.js
@@ -1,11 +1,15 @@
-const { Client, SlashCommandBuilder, GatewayIntentBits } = require('discord.js');
-var ms = require('minestat');
+const {
+  Client,
+  SlashCommandBuilder,
+  GatewayIntentBits,
+} = require("discord.js");
+var ms = require("minestat");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('status')
-		.setDescription('View Minecraft Server Status')
+  data: new SlashCommandBuilder()
+    .setName("status")
+    .setDescription("View Minecraft Server Status")
     .addStringOption((option) =>
       option
         .setName("ip")
@@ -18,9 +22,12 @@ module.exports = {
         .setDescription("Enter the type server port")
         .setRequired(false)
     ),
-	async execute(interaction) {
-    const 
-    
-		await interaction.reply("```"+"My Latency:  " + "/ms"+"```");
-	},
+  async execute(interaction) {
+    const ip = interaction.options.getString("ip");
+    const port = interaction.options.getString("port");
+
+
+    console.log(ms.init(ip, 25565))
+    await interaction.reply("```" + "My Latency:  " + "/ms" + "```");
+  },
 };
