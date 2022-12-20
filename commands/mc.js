c3b7b9cdf50ec6d4c677da05845657360e666068
Glitch (slashdiscord) <none>
2022-12-20 11:07:35 +0000

    😼🐏 Checkpoint ./commands/mc.js:47478600/3739

--- commands/mc.js
+++ commands/mc.js
@@ -2,12 +2,11 @@ const {
   Client,
   SlashCommandBuilder,
   GatewayIntentBits,
-  EmbedBuilder
+  EmbedBuilder,
 } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
-
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -21,26 +20,39 @@ module.exports = {
     ),
   async execute(interaction) {
     const ip = interaction.options.getString("ip");
-    const url = "https://api.mcsrvstat.us/2/"+ip;
+    const url = "https://api.mcsrvstat.us/2/" + ip;
     const getServerStatus = await fetch(url);
     const serverData = await getServerStatus.json();
-    
-    const sfbuff = new Buffer.from(serverData.icon.split(",")[1], "base64");
-    
+
     const generateRandomHexColor = () =>
       `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
 
-  const serverEmbed = new EmbedBuilder()
+    const serverEmbed = new EmbedBuilder()
       .setColor(generateRandomHexColor())
-      .setTitle('Server Status')
-      .setThumbnail(sfbuff)
-    	.addFields(
-		    { name: 'Status', value: serverData.online ? 'Online' : 'Offline' },
-		    { name: 'Players', value: serverData.online ? serverData.players.online + '/' + serverData.players.max : 'N/A' },
-		    { name: 'Version', value: serverData.online ? serverData.version : 'N/A' },
-	)
+      .setTitle("Server Status")
+      .setThumbnail(
+     "https://cdn.discordapp.com/attachments/690148635375435825/1054266142283284510/Slash.png"
+      )
+      .addFields(
+        { name: "Status", value: serverData.online ? "Online" : "Offline", inline: true },
+        {
+          name: "Players",
+          value: serverData.online
+            ? serverData.players.online + "/" + serverData.players.max
+            : "N/A", inline: true
+        },
+        {
+          name: "Version",
+          value: serverData.online ? serverData.version : "N/A",
+        },
+        
+      )
       .setTimestamp()
-      .setFooter({ text: 'Slash', iconURL: 'https://cdn.discordapp.com/attachments/690148635375435825/1054266142283284510/Slash.png' });
+      .setFooter({
+        text: "Slash",
+        iconURL:
+          "https://cdn.discordapp.com/attachments/690148635375435825/1054266142283284510/Slash.png",
+      });
 
     await interaction.reply({ embeds: [serverEmbed] });
   },
