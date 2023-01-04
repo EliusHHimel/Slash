5a14562f18758fb70dc8dc882c1ebaca2a435691
Glitch (slashdiscord) <none>
2023-01-04 05:06:36 +0000

    🌑🏹 Checkpoint ./commands/template.js:47478600/1008

---
+++ commands/template.js
@@ -0,0 +1,16 @@
+const { Client, SlashCommandBuilder, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+module.exports = {
+	data: new SlashCommandBuilder()
+  //set the name of your command
+		.setName('yourCommandName')
+  //set a short description about what your command does 
+		.setDescription('Your Command Description'),
+	async execute(interaction) {
+    // Your feature funtionality codes goes here
+    
+    
+		await interaction.reply(`This is a sample command for this open source project. More details in our GitHub Repository. \n GitHub: https://github.com/EliusHHimel/Slash`);
+	},
+};
