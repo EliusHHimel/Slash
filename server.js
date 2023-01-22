f4c0783eda663c382655d144bbd5b167b46be846
Glitch (slashdiscord) <none>
2023-01-22 07:05:59 +0000

    🍏🌻 Checkpoint ./server.js:47478600/288

--- server.js
+++ server.js
@@ -50,9 +50,14 @@ client.on(Events.InteractionCreate, async interaction => {
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
-  console.log(typeof(client.guilds.cache))
-  let jsonData = client.guilds.cache;
-  
+
+  let jsonData = JSON.stringify(client.guilds.cache);
+    console.log(jsonData)
+  fs.writeFile("test.txt", jsonData, function(err) {
+    if (err) {
+        console.log(err);
+    }
+}); 
   client.user.setActivity(`/help on ${client.guilds.cache.size} Servers with  ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} People`, { type: ActivityType.Playing});
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
