ca78d43a4b13b350785810e05271a07faa087974
Glitch (slashdiscord) <none>
2023-03-10 10:57:19 +0000

    🍶👭 Checkpoint ./commands/match.js:47478600/6905

--- commands/match.js
+++ commands/match.js
@@ -12,61 +12,65 @@ const iccAPI = process.env.ICC_API;
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("match")
-    .setDescription("Get ICC match list")
-  ,
+    .setDescription("Get ICC match list"),
   async execute(interaction) {
-    
-    const url = `${iccAPI}/fixtures?tournamentTypes=I%2CWI&startDate=2023-03-09&endDate=2023-03-11&pageSize=100`
+    const url = `${iccAPI}/fixtures?tournamentTypes=I%2CWI&startDate=2023-03-09&endDate=2023-03-11&pageSize=100`;
     const getMatch = await fetch(url, {
-            "headers": {
-                "accept": "*/*",
-                "accept-language": "en-US,en;q=0.9",
-                "account": "ICC",
-                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "sec-ch-ua": "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"",
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "\"Windows\"",
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "cross-site"
-            },
-            "referrer": "https://www.icc-cricket.com/",
-            "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": null,
-            "method": "GET",
-            "mode": "cors",
-            "credentials": "omit"
-        });
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        account: "ICC",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "sec-ch-ua":
+          '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+      },
+      referrer: "https://www.icc-cricket.com/",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "GET",
+      mode: "cors",
+      credentials: "omit",
+    });
     const matchData = await getMatch.json();
-    console.log(matchData.content[0].scheduleEntry)
-    const embeds =
-    for (let match in matchData.content){
-      const {tournamentLabel, matchLabel, matchID, venue} = {
-      tournamentLabel: match.tournamentLabel,
+    console.log(matchData.content[0].scheduleEntry);
+    const embeds = [];
+    for (let match in matchData.content) {
+      console.log(match)
+      const { tournamentLabel, matchLabel, matchID, venue } = {
+        tournamentLabel: match.tournamentLabel,
         matchLabel: match.label,
-        matchID: match.scheduleEntry.matchid.id,
-        venue: match.scheduleEntry.venue.fullName
-    }
-      
-    }
-    
-    
+        matchID: match.matchid.id? match.matchid.id : match.scheduleEntry.matchid.id,
+        venue: match.scheduleEntry.venue.fullName,
+      };
+      const generateRandomHexColor = () =>
+        `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
 
-    const generateRandomHexColor = () =>
-      `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
-    
-    const scoreEmbed = new EmbedBuilder()
-      .setColor(generateRandomHexColor())
-.setThumbnail('https://cdn.discordapp.com/attachments/690148635375435825/1054266142283284510/Slash.png')
-	.setTitle(tournamentLabel)
-	.addFields(
-		    { name: 'Score', value: `${runs}/${wickets}`, inline: true },
-		    { name: 'Over', value: over, inline: true },
-	)
-	// .setDescription(movieData.Plot)
-      .setTimestamp()
-      .setFooter({ text: 'Slash', iconURL: 'https://cdn.discordapp.com/attachments/690148635375435825/1054266142283284510/Slash.png' });
+      const matchEmbed = new EmbedBuilder()
+        .setColor(generateRandomHexColor())
+        .setThumbnail(
+          "https://cdn.discordapp.com/attachments/690148635375435825/1054266142283284510/Slash.png"
+        )
+        .setTitle(tournamentLabel)
+        .addFields(
+          { name: "Label", value: matchLabel, inline: true },
+          { name: "Venue", value: venue, inline: true },
+          { name: "Match ID", value: matchID, inline: false }
+        )
+        // .setDescription(movieData.Plot)
+        .setTimestamp()
+        .setFooter({
+          text: "Slash",
+          iconURL:
+            "https://cdn.discordapp.com/attachments/690148635375435825/1054266142283284510/Slash.png",
+        });
+      embeds.push(matchEmbed)
+    }
 
-    await interaction.reply({ embeds: [scoreEmbed] });
+    await interaction.reply({ embeds: embeds });
   },
 };
