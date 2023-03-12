aeba7ecc83e17cbeb63399f8317ee8fd2497d0ae
Glitch (slashdiscord) <none>
2023-03-12 04:27:31 +0000

    💷🎈 Checkpoint ./commands/help.js:47478600/210

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports = {
       .setDescription('See all the available command list')
     	.addFields(
 		    { name: 'Ping', value: "Run this command to see the bot ping \n ```/ping```"},
-        { name: 'Score', value: "Run this command `/match` to see the server info \n ```/server```" },
+        { name: 'Score', value: "Run this command `/match` to see todays running match list and then grab the `Match ID` you want to see the score of and run the following command with the matchid to see score \n ```/score matchid:101891```" },
         { name: 'Movie/Series', value: "Run this command to search a movie or series information \n ```/movie title: Interstellar type: movie```" },
         { name: 'Quotes', value: "Run this command to get a random quotes \n ```/quotes```" },
         { name: 'MC Status', value: "Run this command to see minecraft server status \n ```/status ip: hypixel.net```" },
