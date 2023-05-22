942c98ef5c537db9322f92aebf78cd1fed44e109
Glitch (slashdiscord) <none>
2023-05-22 05:26:22 +0000

    formating

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-// Require the necessary discord.js classes
-
-const fs = require('node:fs');
-const path = require('node:path');
-const express = require('express');
-const cors = require('cors');
-const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
+const fs = require("node:fs");
+const path = require("node:path");
+const express = require("express");
+const cors = require("cors");
+const {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  ActivityType,
+} = require("discord.js");
 const token = process.env.TOKEN;
 
 const app = express();
@@ -14,68 +18,86 @@ app.use(express.json());
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
 }
 
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
 });
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
+client.once(Events.ClientReady, (c) => {
+  client.user.setActivity(
+    `/help on ${
+      client.guilds.cache.size
+    } Servers with  ${client.guilds.cache.reduce(
+      (a, g) => a + g.memberCount,
+      0
+    )} People`,
+    { type: ActivityType.Playing }
+  );
 
+  let jsonData = JSON.stringify(client.guilds.cache);
+  // console.log(jsonData)
 
-  client.user.setActivity(`/help on ${client.guilds.cache.size} Servers with  ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} People`, { type: ActivityType.Playing});
+  client.user.setActivity(
+    `/help on ${
+      client.guilds.cache.size
+    } Servers with  ${client.guilds.cache.reduce(
+      (a, g) => a + g.memberCount,
+      0
+    )} People`,
+    { type: ActivityType.Playing }
+  );
 
-	let jsonData = JSON.stringify(client.guilds.cache);
-	// console.log(jsonData)
-
-	client.user.setActivity(`/help on ${client.guilds.cache.size} Servers with  ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} People`, { type: ActivityType.Playing });
-
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 // Log in to Discord with your client's token
 client.login(token);
- 
+
 app.listen(5000, () => {
-    console.log('Running server on port', 5000)
+  console.log("Running server on port", 5000);
 
-app.get("/wakeup", function (request, response) {
-	console.log("i'm awake");
-	response.send("i'm awake")
+  app.get("/wakeup", function (request, response) {
+    console.log("i'm awake");
+    response.send("i'm awake");
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Slash Server Running");
+  });
 });
-
-app.get('/', (req, res) => {
-	res.send('Slash Server Running')
-})
-
-
-})
