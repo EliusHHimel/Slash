1e8b143a3b8eb53ce63b3875ed6be1a41a4f2669
Uthsob Chakraborty <uthsob9@gmail.com>
2023-05-22 10:53:19 +0600

    template file added again

--- commands/template.js
+++ commands/template.js
@@ -1,5 +1,16 @@
-// Generate a random number between 1 and 6
-var randomNumber = Math.floor(Math.random() * 6) + 1;
+const { Client, SlashCommandBuilder, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Output the random number
-console.log(randomNumber);
+module.exports = {
+    data: new SlashCommandBuilder()
+        //set the name of your command, must be lowercase ↓ 
+        .setName('commandname')
+        //set a short description about what your command does ↓  
+        .setDescription('Your Command Description'),
+    async execute(interaction) {
+        // Your feature funtionality code goes here ↓ 
+
+        // send the final output to the end user ↓ 
+        await interaction.reply(`This is a sample command for this open source project. More details in our GitHub Repository. \n GitHub: https://github.com/EliusHHimel/Slash`);
+    },
+};
